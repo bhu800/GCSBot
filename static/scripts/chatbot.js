565c02096c24494e0af4e3b501231c089f019d73
Bhumanyu Goyal <goyalbhu862001@gmail.com>
2020-08-21 01:44:49 +0530

    updated requirements.txt

--- static/scripts/chatbot.js
+++
@@ -1,7 +0,0 @@
-// popovers initialization - on hover
-$('[data-toggle="popover-hover"]').popover({
-    html: true,
-    trigger: 'hover',
-    placement: 'bottom',
-    content: function () { return '<img src="' + $(this).data('img') + '" />'; }
-});
